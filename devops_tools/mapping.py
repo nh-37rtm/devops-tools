3ad981e598ea35dbabc64a3dedfa53df4f754475
heim nicolas <heim.nico@gmail.com>
2025-01-21 00:42:37 +0100

    full refacto

--- devops_tools/mapping.py
+++ devops_tools/mapping.py
@@ -3,15 +3,12 @@ from dataclasses import dataclass, is_dataclass
 
 from queue import SimpleQueue, Queue
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 import inspect
-
 import logging
 
-
 T= t.TypeVar('T')
-
 
 # https://stackoverflow.com/questions/56832881/check-if-a-field-is-typing-optional
 def is_optional(field):
@@ -43,6 +40,8 @@ class MapNode(t.Generic[T]):
     destination_type: t.Type[T]
     value: t.Optional[MapReference] = None
     dict_key_source: t.Optional[str] = None
+    set_reference: t.Callable = field(default= None)
+    
 
 
 type_map: dict[t.Type, t.Callable] = {
@@ -51,185 +50,184 @@ type_map: dict[t.Type, t.Callable] = {
     
 }
 
+def map_list_as_list(node: MapNode,
+                     sub_context: MapNodeContext) -> None:
+    
+    node.value = list()
+    logging.debug("mapping source list to origin list type ...")
+
+    for value in node.source:
+        
+        current_list_type, = t.get_args(node.destination_type)
+        
+        logging.debug(f"mapping attribute of type {current_list_type} (destination type hint) ...")
+        logging.debug(f"instanciating {current_list_type} for deep reference...")
+
+        sub_node = MapNode(
+            source=value, destination_type=current_list_type,
+            context=sub_context)
+        
+    node.context.current_queue.put(sub_node)
+
+
+def map_list_as_tuple(node: MapNode,
+                     sub_context: MapNodeContext) -> None:
+
+    logging.debug("mapping source list to origin list type ...")
+
+    arguments = t.get_args(node.destination_type)
+    arguments_size = len(arguments)
+
+    if node.value is None:
+        node.value = tuple()
+    
+    for current_argument_index in range(0, arguments_size):
+        
+        current_argument_value = node.source[current_argument_index]
+        current_argument_type = arguments[current_argument_index]
+        
+        logging.debug(f"mapping attribute of type {current_argument_type} (destination type hint) ...")
+        logging.debug(f"instanciating {current_argument_type} for deep reference...")
+
+        sub_node = MapNode(
+            source=current_argument_value, destination_type=current_argument_type,
+            context=sub_context)
+
+        node.context.current_queue.put(sub_node)
+    
+def map_untyped_list_to_list(node: MapNode,
+                     sub_context: MapNodeContext) -> None:
+    
+    logging.debug("untyped list to list mapping for node ...")
+    
+    for value in node.source:
+        
+        class_name = type(value)
+        logging.debug(f"mapping attribute of type {class_name} ...")
+        logging.debug(f"instanciating {class_name} for deep reference...")
+
+        sub_node = MapNode(
+            source=value, destination_type=class_name,
+            context=sub_context)
+
+        node.context.current_queue.put(sub_node)
+        
+def map_dict_to_dict(node: MapNode, 
+                    sub_context: MapNodeContext) -> None:
+    
+    # create new dict reference
+    node.value = dict()
+
+    for key, value in node.source.items():
+
+        typing_args = t.get_args(node.destination_type)
+        
+        if len(typing_args) == 2 :
+            key_type, value_type = t.get_args(node.destination_type)
+        else:
+            value_type = dict
+                    
+        logging.debug(f"mapping attribute of type {value_type} ...")
+        
+        sub_node = MapNode(
+            source= value, destination_type=value_type,context=sub_context,
+            dict_key_source= key)
+
+        logging.debug(f"instanciating {value_type} for deep reference...")
+        sub_node.value = value_type()
+        
+        logging.debug("creating dict entry")
+        node.value[key] = sub_node.value
+        
+        node.context.current_queue.put(sub_node)
+
+def map_dict_to_dataclass(node: MapNode, 
+                    sub_context: MapNodeContext) -> None:
+           
+    node.value = node.destination_type()
+
+    for field_name, field in node.destination_type.__dataclass_fields__.items():
+        # assert(field_name in source)
+        if not field_name in node.source:
+            setattr(node.value, field_name, None)
+            continue
+    
+        sub_node = MapNode(
+            source= node.source[field_name], destination_type=field.type,
+            context=sub_context, dict_key_source= field_name)
+        
+        node.context.current_queue.put(sub_node)
+         
+
 def list_source_map(node: MapNode, 
                     sub_context: MapNodeContext) -> None:
     
     origin_type = t.get_origin(node.destination_type)
     
-    if origin_type is not None and \
-            issubclass(origin_type, list) :
-                
-        current_list_node: t.List = list()
-                
-        logging.debug("mapping source list to origin list type ...")
-
-        for value in node.source:
+    match origin_type:
+        case origin_type \
+            if origin_type is not None \
+                and issubclass(origin_type, list):
             
-            current_list_type, = t.get_args(node.destination_type)
-            
-            logging.debug(f"mapping attribute of type {current_list_type} (destination type hint) ...")
-            logging.debug(f"instanciating {current_list_type} for deep reference...")
-
-            sub_node = MapNode(
-                source=value, destination_type=current_list_type,
-                context=sub_context)
-            
-            node.value = current_list_node
-            
-            node.context.current_queue.put(sub_node)
-            
-        return
-            
-    if  inspect.isclass(node.destination_type) and \
-            issubclass(node.destination_type, list):
+            map_list_as_list(node, sub_context)            
     
-        logging.debug("untyped list to list mapping for node ...")
-        
-        for value in node.source:
-            
-            class_name = type(value)
-            logging.debug(f"mapping attribute of type {class_name} ...")
-            logging.debug(f"instanciating {class_name} for deep reference...")
-            # sub_context.reference = class_name()
-
-            sub_node = MapNode(
-                source=value, destination_type=class_name,
-                context=sub_context)
-
-            # sub_node.value = class_name()            
-            node.context.current_queue.put(sub_node)
-
-
-def tuple_source_map(node: MapNode, 
-                    sub_context: MapNodeContext) -> None:
+        case origin_type \
+            if origin_type is not None \
+                and issubclass(origin_type, tuple):
+                    
+            map_list_as_tuple(node, sub_context)
     
-    origin_type = t.get_origin(node.destination_type)
-    
-    if origin_type is not None and \
-            issubclass(origin_type, tuple) :
-                
-        current_list_node: t.Tuple = tuple()
-                
-        logging.debug("mapping source list to origin list type ...")
+        case node.destination_type \
+            if inspect.isclass(node.destination_type) and \
+                issubclass(node.destination_type, list):
+            map_untyped_list_to_list(node, sub_context)
 
-        arguments = t.get_args(node.destination_type)
-        
-        arguments_size = len(arguments)
-        
-        for current_argument_index in range(0, arguments_size):
-            
-            current_argument_value = node.source[current_argument_index]
-            current_argument_type = arguments[current_argument_index]
-            
-            logging.debug(f"mapping attribute of type {current_argument_type} (destination type hint) ...")
-            logging.debug(f"instanciating {current_argument_type} for deep reference...")
-
-            sub_node = MapNode(
-                source=current_argument_value, destination_type=current_argument_type,
-                context=sub_context)
-            
-            sub_node.context.parent_node = node
-            
-            node.value = current_list_node
-            node.context.current_queue.put(sub_node)
-            
-            
-        return
-            
-    if  inspect.isclass(node.destination_type) and \
-            issubclass(node.destination_type, list):
-    
-        logging.debug("untyped list to list mapping for node ...")
-        
-        for value in node.source:
-            
-            class_name = type(value)
-            logging.debug(f"mapping attribute of type {class_name} ...")
-            logging.debug(f"instanciating {class_name} for deep reference...")
-            # sub_context.reference = class_name()
-
-            sub_node = MapNode(
-                source=value, destination_type=class_name,
-                context=sub_context)
-
-            # sub_node.value = class_name()            
-            node.context.current_queue.put(sub_node)
-       
 
 def dict_source_map(node: MapNode, 
                     sub_context: MapNodeContext) -> None:
     
     origin_type = t.get_origin(node.destination_type)
     
-    if origin_type is not None and \
+    match origin_type:
+        case origin_type if origin_type is not None and \
         issubclass( origin_type, dict ):
+            map_dict_to_dict(node, sub_context)
 
-        # create new dict reference
-        node.value = dict()
+        case node.destination_type \
+            if is_dataclass( node.destination_type ):    
+            map_dict_to_dataclass(node, sub_context)
 
-        for key, value in node.source.items():
-
-                        
-            typing_args = t.get_args(node.destination_type)
-            
-            if len(typing_args) == 2 :
-                key_type, value_type = t.get_args(node.destination_type)
-            else:
-                value_type = dict
-                        
-            logging.debug(f"mapping attribute of type {value_type} ...")
-            
-            sub_node = MapNode(
-                source= value, destination_type=value_type,context=sub_context,
-                dict_key_source= key)
-            
-            logging.debug(f"instanciating {value_type} for deep reference...")
-            sub_node.value = value_type()
-            
-            logging.debug("creating dict entry")
-            node.value[key] = sub_node.value
-            
-            node.context.current_queue.put(sub_node)
-            
-        return            
-
-    if is_dataclass( node.destination_type ):
-        
-        node.value = node.destination_type()
-
-        for field_name, field in node.destination_type.__dataclass_fields__.items():
-            # assert(field_name in source)
-            if not field_name in node.source:
-                setattr(node.value, field_name, )
-                continue
-
-            
-            sub_node = MapNode(
-                source= node.source[field_name], destination_type=field.type,
-                context=sub_context, dict_key_source= field_name)
-            
-            node.context.current_queue.put(sub_node)
-            
 def populate_parent(node: MapNode) -> None:
-    if node.context.parent_node is None:
+
+    parent_node = node.context.parent_node
+
+    if parent_node is None:
         return
     
-    if is_dataclass( node.context.parent_node.value ):    
+    if is_dataclass( parent_node.value ):    
         if node.dict_key_source is not None:
-            node.context.parent_node.value.__dict__[node.dict_key_source] = node.value
+            logging.info(f'setting attribute {node.dict_key_source}')
+            parent_node.value.__dict__[node.dict_key_source] = node.value
+            if node.dict_key_source == 't':
+                pass
+            if node.dict_key_source == 'd_a':
+                pass
         return
             
-    if issubclass( type(node.context.parent_node.value), dict):
+    if issubclass( type(parent_node.value), dict):
         if node.dict_key_source is not None:
-            node.context.parent_node.value[node.dict_key_source] = node.value
+            logging.info(f'setting attribute {node.dict_key_source} to {node.value}')
+            parent_node.value[node.dict_key_source] = node.value
         return
             
-    if issubclass( type(node.context.parent_node.value), list):
-        node.context.parent_node.value.append( node.value )
+    if issubclass( type(parent_node.value), list):
+        parent_node.value.append( node.value )
         return
     
-    if issubclass( type(node.context.parent_node.value), tuple):
-        node.context.parent_node.value = node.context.parent_node.value + (node.value,)
+    if issubclass( type(parent_node.value), tuple):
+        parent_node.value = parent_node.value + (node.value,)        
+        populate_parent(parent_node)
+            
         return
 def walk_through(node: MapNode) -> None:
     
@@ -237,20 +235,23 @@ def walk_through(node: MapNode) -> None:
     
     
     while (True):
-        if isinstance(node.source, list):
+        if isinstance(node.source, list) :
             list_source_map(node, sub_context)
             break
 
         if isinstance(node.source, dict) :      
             dict_source_map(node, sub_context)
             break
-        
-        if isinstance(node.source, tuple) :      
-            tuple_source_map(node, sub_context)
+
+        if isinstance(node.source, tuple) :
+            list_source_map(node, sub_context)
             break
 
         # other types are not supported and affected as is
+        logging.info(f'setting raw value to {node.source}')
+        
         node.value = node.source
+            
         break
 
     populate_parent(node)
--- tests/test_global.py
+++ tests/test_global.py
@@ -5,6 +5,7 @@ import logging
 import typing as t
 
 from dataclasses import dataclass
+import dataclasses
 
 from pydantic_partial import create_partial_model
 
@@ -13,23 +14,20 @@ import pydantic
 
 @dataclass
 class ClassB:
-
-    def __init__(self):
-        pass
-
-    b_a: str
-    b_b: str
+    b_a: str = dataclasses.field(default=None)
+    b_b: str = dataclasses.field(default=None)
 
 
 @dataclass
 class ClassC:
+    c_a: str = dataclasses.field(default=None)
+    c_b: str = dataclasses.field(default=None)
 
-    def __init__(self):
-        pass
 
-    c_a: str
-    c_b: str
-
+@dataclass
+class ClassD:
+    d_a: t.Tuple[str, str, ClassB] = dataclasses.field(default=None)
+    d_b: t.List[str] = dataclasses.field(default=None)
 
 @dataclass
 class ClassA:
@@ -82,7 +80,7 @@ def fix01() -> dict:
 
 @fixture(name="source_data2")
 def fix03() -> t.Tuple:
-    return (
+    return [
         "toto",
         None,
         {
@@ -98,7 +96,11 @@ def fix03() -> t.Tuple:
             "o_ac": {"c_a": "a c string", "c_b": "another c string"},
             "t": ["1", "2", "3"],
         },
-    )
+    ]
+
+@fixture(name="list_test")
+def fix04() -> t.List:
+    return [ 1, 2, 3, ClassB()]
 
 
 @fixture(name="source_data_list")
@@ -151,6 +153,23 @@ def test_map_tuple(logger: logging.Logger, source_data2: dict):
     logger.info(source_data2)
 
     result = m.deep_map_from_raw(source_data2, t.Tuple[str, str, ClassA])
+    pass
+    logging.info(result)
+
+    pass
+
+
+
+def test_map_tuple_2(logger: logging.Logger):
+    logger.info("starting tests ...")
+
+    result = m.deep_map_from_raw( ("a", "b", 
+                                   
+                                   { 
+                                    "d_a":( "1", "2", { "b_a": "b_a" }), 
+                                    "d_b":[ "A", "B", "C"] 
+                                   },
+                                   ), t.Tuple[str, str, ClassD])
     pass
     logging.info(result)
 
